5fd0960c1c35747cd6b035f5d7a4c014aeeaa66e
Kurookku <40919531+Kurookku@users.noreply.github.com>
2024-09-14 13:52:25 -0400

    testing

--- routes/route_index.js
+++ routes/route_index.js
@@ -15,7 +15,7 @@ router.get('/', async function(request, response, next) {
   response.setHeader('Cache-Control', 'public, max-age=30');
   response.status(200);
 
-  response.send("Welcome Hokies!")
+  response.send("Welcome Hokies!!!")
 
   // response.render(path.join(app.get('views'), 'index.pug'), {
   //     title: request.app.locals.site_name
